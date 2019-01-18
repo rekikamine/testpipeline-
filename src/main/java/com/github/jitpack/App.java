4a2ca92657715bc3aa62ef6ee41f7a07d2ceee6d
rekikamine <mohamed.amine.rekik@gmail.com>
2019-01-18 16:22:05 +0100

    first test

---
+++ src/main/java/com/github/jitpack/App.java
@@ -0,0 +1,14 @@
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Hamda");
+    }
+
+}
